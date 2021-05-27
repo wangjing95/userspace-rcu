a0c24b4b413b41e89873a584ebc83cd866572ec3
loongson <loongson@loongson-pc>
2021-05-27 11:09:44 +0800

    Add loongarch64 support

---
+++ include/urcu/arch/loongarch64.h
@@ -0,0 +1,43 @@
+#ifndef _URCU_ARCH_LOONGARCH64_H
+#define _URCU_ARCH_LOONGARCH64_H
+
+/*
+ * arch_loongarch64.h: trivial definitions for the LOONGARCH64 architecture.
+ *
+ * Copyright (c) 2010 Paolo Bonzini <pbonzini@redhat.com>
+ * Copyright (c) 2012 Ralf Baechle <ralf@linux-loongarch64.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <urcu/compiler.h>
+#include <urcu/config.h>
+#include <urcu/syscall-compat.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define cmm_mb()			__asm__ __volatile__ (		    \
+					"	dbar 0			\n" \
+					:::"memory")
+
+#ifdef __cplusplus
+}
+#endif
+
+#include <urcu/arch/generic.h>
+
+#endif /* _URCU_ARCH_LOONGARCH64_H */
